5adfa728ffbd42814a2421259de2a6b7465c093e
jesus gil <alejogilrey@gmail.com>
2026-01-29 23:47:44 -0400

    feat: agregar más mensajes al programa

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
-    cout << "Mi nombre es: [Tu Nombre]" << endl;
+    cout << "Mi nombre es: Jesus Gil" << endl;
+    cout << "Estoy aprendiendo Git en Programación II" << endl;
+    cout << "Este es mi segundo commit!" << endl;
     return 0;
 }
